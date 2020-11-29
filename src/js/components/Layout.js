c2a2770892e5a60ff4773fdb69b24e16486e89f8
haragumi <uehara1902@gmail.com>
2020-11-29 18:21:28 +0900

    initial commit

---
+++ src/js/components/Layout.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+export default class Layout extends React.Component {
+  render() {
+    const title = "Welcome Kampala!";
+    return (
+      <div>
+        <Header title={title} />
+        <Footer />
+      </div>
+    );
+  }
+}
